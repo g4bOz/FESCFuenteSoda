2bc488281bb9c021ff88251d2fca809c257332d8
g4bOz <gr3863165@gmail.com>
2024-10-17 23:33:00 -0500

    Se agrega el Proyecto

---
+++ Frontend/frontend/src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+export const routes: Routes = [
+
+    { path: 'login', component: LoginPageComponent },
+    {
+        path: '', children: [
+            {
+                path: '', component: HomePageComponent,
+
+            },
+        ],
+    },
+];
